b7b655ae63155884223c3d4b50edeba1d022b332
mitanshi25 <92587605+mitanshi25@users.noreply.github.com>
2023-08-04 19:09:44 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+// let aString = "mitanshi"
+
+
+// console.log(aString)
+// console.log(typeof aString)
+
+// let convertedaString = Number(aString)
+
+// console.log(convertedaString)
+// console.log(typeof convertedaString)
+
+
+
+let aBoolean = undefined
+
+let convertedaBoolean = Number(aBoolean)
+
+console.log(typeof convertedaBoolean)
+
+console.log(convertedaBoolean)
+
+
+
+
+
+
+
+/* 
+1-we can not convert a string in a number so it will give (NAN) when we do console.log(convertedaString)
+2- when we concvert a boolean value in a number then we will get 1,0 as a accepted value and the type will be number
+3- when we convert a undefined value in a number then the type will be number but the converted value will be (NaN)=> not a number*/
