04b6fa43ea695060db8e6bf1aeb058a528d08007
mitanshi25 <92587605+mitanshi25@users.noreply.github.com>
2023-08-04 18:45:45 +0000

    learned about variables and data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+"Use strict"; //treat all JS code as newer version
+
+//datatypes
+
+let name = "Mitanshi" //string
+let age = 18 //number
+
+
+//number ==> 2^53
+//bigint ==>
+
+//string
+
+let fName = "mitanshi"
+let lName = 'maheshwari' //both are valid
+
+//null ==> standalone value
+
+//undefined ==> when we haven't provide the value like ==>let college;
+
+//symbol ==> for the uniqueness
+
+console.log(typeof fName)
+
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
+
+
